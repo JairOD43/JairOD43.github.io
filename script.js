23f384e3e11a6653e6fd9754ac31c4e816673514
Jair Ortega <jairthink16@gmail.com>
2024-07-16 14:50:30 -0600

    Correcione

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
+/* JavaScript code */
 function classToggle() {
-    var el = document.querySelector('.icon-cards__content');
-    el.classList.toggle('step-animation');
-  }
-  
-  document.querySelector('#toggle-animation').addEventListener('click', classToggle);
+  var el = document.querySelector('.icon-cards__content');
+  el.classList.toggle('step-animation');
+}
 
-  const blobCursor = (() => {  
-    const cursor = document.querySelector('#cursorBlob');
-    const links = document.querySelectorAll('.nav__link');
-    const setCursorPos = (e) => {
-      const { pageX: posX, pageY: posY } = e;
-      cursor.style.top = `${posY - (cursor.offsetHeight / 2)}px`;
-      cursor.style.left = `${posX - (cursor.offsetWidth / 2)}px`;
-    };
-    document.addEventListener('mousemove', setCursorPos);
-    
-    const setCursorHover = () => cursor.style.transform = 'scale(2.5)';
-    const removeCursorHover = () => cursor.style.transform = '';
-    links.forEach(link => link.addEventListener('mouseover', setCursorHover));
-    links.forEach(link => link.addEventListener('mouseleave', removeCursorHover));  
-  })();  
+document.querySelector('#toggle-animation').addEventListener('click', classToggle);
+
+const blobCursor = (() => {  
+  const cursor = document.querySelector('#cursorBlob');
+  const links = document.querySelectorAll('.nav__link');
+  const setCursorPos = (e) => {
+    const { pageX: posX, pageY: posY } = e;
+    cursor.style.top = `${posY - (cursor.offsetHeight / 2)}px`;
+    cursor.style.left = `${posX - (cursor.offsetWidth / 2)}px`;
+  };
+  document.addEventListener('mousemove', setCursorPos);
+  
+  const setCursorHover = () => cursor.style.transform = 'scale(2.5)';
+  const removeCursorHover = () => cursor.style.transform = '';
+  links.forEach(link => link.addEventListener('mouseover', setCursorHover));
+  links.forEach(link => link.addEventListener('mouseleave', removeCursorHover));  
+})();
