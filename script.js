477c07b2924ee13fcfc7c9878d8d836fe6230a64
Jair Ortega <jairthink16@gmail.com>
2024-07-16 14:43:35 -0600

    arreglos

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 function classToggle() {
-  var el = document.querySelector('.icon-cards__content');
-  el.classList.toggle('step-animation');
-}
-
-document.querySelector('#toggle-animation').addEventListener('click', classToggle);
-const imgContent = document.querySelectorAll('.img-content-hover');
-
-function showImgContent(e) {
-  for(var i = 0; i < imgContent.length; i++) {
-    x = e.pageX;
-    y = e.pageY;
-    imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
+    var el = document.querySelector('.icon-cards__content');
+    el.classList.toggle('step-animation');
   }
-};
+  
+  document.querySelector('#toggle-animation').addEventListener('click', classToggle);
 
-document.addEventListener('mousemove', showImgContent);
+  const blobCursor = (() => {  
+    const cursor = document.querySelector('#cursorBlob');
+    const links = document.querySelectorAll('.nav__link');
+    const setCursorPos = (e) => {
+      const { pageX: posX, pageY: posY } = e;
+      cursor.style.top = `${posY - (cursor.offsetHeight / 2)}px`;
+      cursor.style.left = `${posX - (cursor.offsetWidth / 2)}px`;
+    };
+    document.addEventListener('mousemove', setCursorPos);
+    
+    const setCursorHover = () => cursor.style.transform = 'scale(2.5)';
+    const removeCursorHover = () => cursor.style.transform = '';
+    links.forEach(link => link.addEventListener('mouseover', setCursorHover));
+    links.forEach(link => link.addEventListener('mouseleave', removeCursorHover));  
+  })();  
