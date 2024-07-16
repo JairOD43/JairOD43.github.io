5c5b51a1257b91bc94ad2542aa284b2255cebbb8
Jair Ortega <jairthink16@gmail.com>
2024-07-16 14:35:42 -0600

    agregar galleria

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function classToggle() {
 }
 
 document.querySelector('#toggle-animation').addEventListener('click', classToggle);
+const imgContent = document.querySelectorAll('.img-content-hover');
+
+function showImgContent(e) {
+  for(var i = 0; i < imgContent.length; i++) {
+    x = e.pageX;
+    y = e.pageY;
+    imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
+  }
+};
+
+document.addEventListener('mousemove', showImgContent);
