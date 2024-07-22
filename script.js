eca431aa6d5cad9bc169b54571cfbf2adb030fd5
Jair Ortega <jairthink16@gmail.com>
2024-07-22 01:11:59 -0600

    scrip prueba y link

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const targetId = this.getAttribute('href').substring(1);
         const targetElement = document.getElementById(targetId);
         window.scrollTo({
-          top: targetElement.offsetTop - 50, // Adjusted offset for better alignment
+          top: targetElement.offsetTop - 50, 
           behavior: 'smooth'
         });
       });
